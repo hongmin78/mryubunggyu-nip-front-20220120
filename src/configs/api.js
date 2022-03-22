4a46345bb1ff173bba7243549e8cf0a3828a909a
Saddam-tech <salokhiddinov0727@gmail.com>
2022-03-22 10:17:41 +0900

    ls

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,26 +1,24 @@
-
 // const URL='http://nip s1.net:348 15'
 // const URL='http://3.35. 117.87:34 815'
-const URL='https://nftinfinity.world:34825'
+const URL = "https://nftinfinity.world:34825";
 
-const API={
-		API_MAX	: URL + `/queries/max` // /:tablename/:fieldname
-	, API_TXS	: URL + '/transactions' // /:txhash
-	, API_TICKERS : URL + '/tickers'
-	, API_USERINFO : `${URL}/users/info`
-	, API_TOGGLE_FAVORITE : URL + '/favorites/toggle'
-	, API_LOGIN : URL + '/users/login'
-	, API_EMAIL_REQUEST : URL + '/signup/email/request'
-	, API_SIGNUP : URL + '/signup/signup'
-	, API_QUERY_REFERER : URL + '/queries/singlerow' // /:tablename/:fieldname/:fieldval
-	, API_QUERY_USERADDRESS : URL + '/queries/singlerow' 
-	, API_QUERY_SINGLEROW : URL + '/queries/singlerow'
-	, API_PREMIUMITEMS : URL + '/queries/rows'
-	, API_COMMONITEMS  : URL + '/queries/rows'
-	, API_ITEMDETAIL : URL + '/items/item' // /:itemid
-	, API_EMAIL_VERIFY: `${URL}/signup/email/auth`
-}
+const API = {
+  API_MAX: URL + `/queries/max`, // /:tablename/:fieldname
+  API_TXS: URL + "/transactions", // /:txhash
+  API_TICKERS: URL + "/tickers",
+  API_USERINFO: `${URL}/users/info`,
+  API_TOGGLE_FAVORITE: URL + "/favorites/toggle",
+  API_LOGIN: URL + "/users/login",
+  API_EMAIL_REQUEST: URL + "/signup/email/request",
+  API_SIGNUP: URL + "/signup/signup",
+  API_QUERY_REFERER: URL + "/queries/singlerow", // /:tablename/:fieldname/:fieldval
+  API_QUERY_USERADDRESS: URL + "/queries/singlerow",
+  API_QUERY_SINGLEROW: URL + "/queries/singlerow",
+  API_PREMIUMITEMS: URL + "/queries/rows",
+  API_COMMONITEMS: URL + "/queries/rows",
+  API_ITEMDETAIL: URL + "/items/item", // /:itemid
+  API_EMAIL_VERIFY: `${URL}/signup/email/auth`,
+  API_RECEIVABLES: `${URL}/queries/receivables/username`,
+};
 
-export { 
-	API
-}
+export { API };
