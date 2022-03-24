d185b298588c2b2713e3d16e115cfab8e20b0c7d
psysever <wlfah99@gmail.com>
2022-03-24 12:23:57 +0900

    pull sam

--- src/configs/addresses.js
+++ src/configs/addresses.js
@@ -7,6 +7,7 @@ const jaddresses = {
     contract_stake: "0xc964f791b8e92336abfb3fde5fd4738700110379",
     contract_admin: "0x005b38d678753f211abae8dbb38c45a9d159ecee", // 0xd24ff65996e94d79d3cd8a22e8f95c42fbec0e0f'
     contract_pay_for_assigned_item: "0xaf9764ef97102951f9339d515e4c41099e8fc992",
+    payment_for_delinquency: "0x2cdb6d037cc0a9e5480f9071b4a00f20055057a7",
   },
   BSC_MAINNET: {
     contract_USDT: "0x55d398326f99059fF775485246999027B3197955", // owner : 0x83f714ad20e34748516e8367faf143abde6c3783
