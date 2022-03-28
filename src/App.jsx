cfecccc8fab8856d75afc52f51c3e814ba636dc2
psysever <wlfah99@gmail.com>
2022-03-28 19:28:40 +0900

    20220328

--- src/App.jsx
+++ src/App.jsx
@@ -39,54 +39,60 @@ function App() {
   useEffect(
     (_) => {
       const queryuseraddress = (address) => {
-        axios
-          .get(API.API_QUERY_USERADDRESS + `/users/username/${address}`)
-          .then((resp) => {
-            LOGGER("QlzCkJ0KYu", resp.data);
-            let { status, respdata } = resp.data;
-            if (status == "OK") {
-              if (respdata?.id) {
-                dispatch(setaddress(address));
-                dispatch(setLogin(address));
-                setaddress(address);
-              }
-            } else {
-              LOGGER("user not found");
+        axios.get(API.API_QUERY_USERADDRESS + `/users/username/${address}`).then((resp) => {
+          LOGGER("QlzCkJ0KYu", resp.data);
+          let { status, respdata } = resp.data;
+          if (status == "OK") {
+            if (respdata?.id) {
+              dispatch(setaddress(address));
+              dispatch(setLogin(address));
+              setaddress(address);
             }
-          });
+          } else {
+            LOGGER("user not found");
+          }
+        });
       };
       let { ethereum } = window;
       if (ethereum) {
+        ethereum.on("accountsChanged", (resp) => {
+          LOGGER("GsnRPWi8Zg@accountsChanged", resp);
+          SetErrorBar(messages.MSG_ACCOUNTS_CHANGED);
+          if (resp[0]) {
+            let address = resp[0];
+            dispatch(setaddress(address));
+            dispatch(setLogin(address));
+            setaddress(address);
+            dispatch(setLogin(address));
+          } else {
+            dispatch(setaddress(null));
+            dispatch(setLogin(null));
+            dispatch(setLogin(null));
+            setaddress(null);
+          }
+        });
+        ethereum.on("networkChanged", function (networkId) {
+          LOGGER(networkId);
+          // Time to reload your interface with the new networkId
+        });
+        ethereum.on("chainChanged", (chainId) => {
+          LOGGER("@chainChanged", chainId);
+        });
       } else {
-        return;
+        SetErrorBar("Please Install MetaMask");
+        window.open(
+          "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ko",
+          "_blank"
+        );
       }
       let { selectedAddress: address } = ethereum;
       if (address) {
         queryuseraddress(address);
+
+        //		dispatch( setaddress(  ) )  // strDot(ethereum.selectedAddress , 8 , 0 ) )
       } else {
+        SetErrorBar("Please Connect Wallet");
       }
-      ethereum.on("accountsChanged", (resp) => {
-        LOGGER("GsnRPWi8Zg@accountsChanged", resp);
-        SetErrorBar(messages.MSG_ACCOUNTS_CHANGED);
-        if (resp[0]) {
-          let address = resp[0];
-          dispatch(setaddress(address));
-          dispatch(setLogin(address));
-          setaddress(address);
-        } else {
-          dispatch(setaddress(null));
-          dispatch(setLogin(null));
-          setaddress(null);
-        }
-      });
-      ethereum.on("networkChanged", function (networkId) {
-        LOGGER(networkId);
-        // Time to reload your interface with the new networkId
-      });
-      ethereum.on("chainChanged", (chainId) => {
-        LOGGER("@chainChanged", chainId);
-      });
-      //		dispatch( setaddress(  ) )  // strDot(ethereum.selectedAddress , 8 , 0 ) )
     },
     [window.ethereum]
   );
@@ -110,23 +116,13 @@ function App() {
   return (
     <AppBox>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
         rel="stylesheet"
       />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:wght@500&display=swap"
-        rel="stylesheet"
-      />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:wght@500&display=swap" rel="stylesheet" />
 
       <GlobalStyle />
       <HashRouter>
@@ -139,10 +135,7 @@ function App() {
 
           <Route path="/connectwallet" element={<ConnectWallet />} />
           <Route path="/connectwallet/:popup" element={<ConnectWallet />} />
-          <Route
-            path="/emailauth/:email/:authNum/:walletaddress"
-            element={<EmailAuth />}
-          />
+          <Route path="/emailauth/:email/:authNum/:walletaddress" element={<EmailAuth />} />
           <Route path="/emailauth/:email/:authNum" element={<EmailAuth />} />
 
           <Route path="/staking" element={<Staking />} />
@@ -154,10 +147,7 @@ function App() {
 
           <Route path="/market" element={<Market />} />
           <Route path="/market/detail/:itemid" element={<MarketDetail />} />
-          <Route
-            path="/market/detail/:itemid/:popup"
-            element={<MarketDetail />}
-          />
+          <Route path="/market/detail/:itemid/:popup" element={<MarketDetail />} />
 
           <Route path="/mypage" element={<Mypage />} />
           <Route path="/editprof" element={<EditProf />} />
