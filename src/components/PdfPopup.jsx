47cda8168a22a4d2cbd0309ca0532a3cd0f51e37
Saddam-tech <salokhiddinov0727@gmail.com>
2022-06-25 00:28:28 +0900

    dynamically outputting manuals

--- src/components/PdfPopup.jsx
+++ src/components/PdfPopup.jsx
@@ -19,25 +19,6 @@ import nip_wp17 from "../doc/nip_wp17.png";
 import nip_wp18 from "../doc/nip_wp18.png";
 import nip_wp19 from "../doc/nip_wp19.png";
 
-//maunal
-import nip_manu01 from "../doc/nftinfinity_manual/nip_manu-01.png";
-import nip_manu02 from "../doc/nftinfinity_manual/nip_manu-02.png";
-import nip_manu03 from "../doc/nftinfinity_manual/nip_manu-03.png";
-import nip_manu04 from "../doc/nftinfinity_manual/nip_manu-04.png";
-import nip_manu05 from "../doc/nftinfinity_manual/nip_manu-05.png";
-import nip_manu06 from "../doc/nftinfinity_manual/nip_manu-06.png";
-import nip_manu07 from "../doc/nftinfinity_manual/nip_manu-07.png";
-import nip_manu08 from "../doc/nftinfinity_manual/nip_manu-08.png";
-import nip_manu09 from "../doc/nftinfinity_manual/nip_manu-09.png";
-import nip_manu10 from "../doc/nftinfinity_manual/nip_manu-10.png";
-import nip_manu11 from "../doc/nftinfinity_manual/nip_manu-11.png";
-import nip_manu12 from "../doc/nftinfinity_manual/nip_manu-12.png";
-import nip_manu13 from "../doc/nftinfinity_manual/nip_manu-13.png";
-import nip_manu14 from "../doc/nftinfinity_manual/nip_manu-14.png";
-import nip_manu15 from "../doc/nftinfinity_manual/nip_manu-15.png";
-import nip_manu16 from "../doc/nftinfinity_manual/nip_manu-16.png";
-import nip_manu17 from "../doc/nftinfinity_manual/nip_manu-17.png";
-
 export default function PdfPopup({ type }) {
   //manual == "" introduction == ""
   console.log("type", type);
@@ -45,7 +26,7 @@ export default function PdfPopup({ type }) {
     <>
       {type === "introduction" && (
         <PdfPopupBox>
-          <img src={nip_wp01} alt="" />
+          {/* <img src={nip_wp01} alt="" />
           <img src={nip_wp02} alt="" />
           <img src={nip_wp03} alt="" />
           <img src={nip_wp04} alt="" />
@@ -63,28 +44,34 @@ export default function PdfPopup({ type }) {
           <img src={nip_wp16} alt="" />
           <img src={nip_wp17} alt="" />
           <img src={nip_wp18} alt="" />
-          <img src={nip_wp19} alt="" />
+          <img src={nip_wp19} alt="" /> */}
+
+          {Array.from(19).forEach((el, i) => {
+            return (
+              <img
+                src={require(`../doc/nip_wp${
+                  i < 10 ? "0" + i + 1 : i + 1
+                }.png`)}
+                alt=""
+                key={i}
+              />
+            );
+          })}
         </PdfPopupBox>
       )}
       {type === "manual" && (
         <PdfPopupBox>
-          <img src={nip_manu01} alt="" />
-          <img src={nip_manu02} alt="" />
-          <img src={nip_manu03} alt="" />
-          <img src={nip_manu04} alt="" />
-          <img src={nip_manu05} alt="" />
-          <img src={nip_manu06} alt="" />
-          <img src={nip_manu07} alt="" />
-          <img src={nip_manu08} alt="" />
-          <img src={nip_manu09} alt="" />
-          <img src={nip_manu10} alt="" />
-          <img src={nip_manu11} alt="" />
-          <img src={nip_manu12} alt="" />
-          <img src={nip_manu13} alt="" />
-          <img src={nip_manu14} alt="" />
-          <img src={nip_manu15} alt="" />
-          <img src={nip_manu16} alt="" />
-          <img src={nip_manu17} alt="" />
+          {Array.from(17).forEach((el, i) => {
+            return (
+              <img
+                src={require(`../doc/nftinfinity_manual/nip_wp${
+                  i < 10 ? "0" + i + 1 : i + 1
+                }.png`)}
+                alt=""
+                key={i}
+              />
+            );
+          })}
         </PdfPopupBox>
       )}
     </>
